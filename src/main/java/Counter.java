0b3883d4615d471a8a9d705f36a42001f2da44a1
Hilmi Fauziyyah <126895059+HilmiFauziyyah@users.noreply.github.com>
2024-05-31 07:47:25 +0700

    Update Counter.java

--- src/main/java/Counter.java
+++ src/main/java/Counter.java
@@ -2,26 +2,30 @@ package src.main.java;
 
 public class Counter {
 
-	private int count;
-	
-	public Counter() {
-		this.reset();
-	}
-		
-	public void reset() {
-		count = 0;
-	}
-	
-	public void increment() {
-		count++;
-	}
-	
-	public void decrement() {
-		count--;
-	}
-	
-	public int getCount() {
-		return count;
-	}
-	
+    private int count;
+
+    public Counter() {
+        this.reset();
+    }
+
+    public void reset() {
+        count = 1; // Set initial value to 1 to avoid division by zero
+    }
+
+    public void divide(int value) {
+        if (value != 0) {
+            count /= value;
+        } else {
+            System.out.println("Cannot divide by zero");
+        }
+    }
+
+    public void multiply(int value) {
+        count *= value;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
 }
